4c69ede8b49e4bf368713c00bb1c21e009d6352a
brunofelipe00 <brunofelipesobrinho@gmail.com>
2024-08-26 17:16:20 -0300

    Add files via upload

---
+++ 04.Atividade.B.sql
@@ -0,0 +1,13 @@
+/*Funções e Operadores Numericos SQL*/
+select 1;
+select 1 + 1;
+select - (2);
+select 4/3;
+select 10 div 2;
+select abs(-2);
+select pow(2 , 3);
+select * from tb_cli
+where cli_nome like 'Ana%';
+/*Funções Data e Hora MySQL*/
+select now();
+select day('2020-09-21 23:33:25');
